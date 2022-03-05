71f54ce9a3cf881e97b68dc685d34fcd4bfbe66f
Sherwin Robert <sherwinrobert3150@gmail.com>
2022-03-05 09:19:30 +0530

    first commit

---
+++ src/component/Answers.js
@@ -0,0 +1,14 @@
+
+class Answers extends React.Component{
+
+    render(){
+
+        return(
+            <div className="ans">{this.props.answers}</div>
+        )
+    }
+
+}
+
+
+export default Answers;
